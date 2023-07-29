5f94cbf2262d87a6e19c7c97f1b7196b6b27555c
WEBkit003 <140748017+WEBkit003@users.noreply.github.com>
2023-07-29 22:37:28 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Modal functionality
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the Bootstrap modal element by ID
+    var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+
+    // Show the modal
+    myModal.show();
+});
